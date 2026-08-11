1a8b1c087ee9c13772d1caa52d6e37428e2c8f46
Luke <luke@42galaxies.studio>
2026-08-10 23:18:07 -0700

    Feat: Connect terminal module to application loop

--- src/main.c
+++ src/main.c
@@ -1,10 +1,23 @@
+#include "panel/terminal.h"
+#include <ncurses.h>
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char **argv)
-{
-    (void)argc;
-    (void)argv;
-    printf("flecs-explorer v0.1.0-mvp initialized\n");
+int main(void) {
+    if (PanelTerminalInit() != PANEL_OK) {
+        fprintf(stderr, "Failed to initialize terminal.\n");
+        return EXIT_FAILURE;
+    }
+
+    int ch;
+    while (1) {
+        PanelTerminalDrawLayout();
+        ch = getch();
+        if (PanelTerminalHandleInput(ch)) {
+            break;
+        }
+    }
+
+    PanelTerminalCleanup();
     return EXIT_SUCCESS;
 }
